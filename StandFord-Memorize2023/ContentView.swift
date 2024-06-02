969ae2850b014452aca303f3316f856f7382bb0a
Wise Wong <imhwc@icloud.com>
2024-06-02 15:21:44 +0800

    更改一下文字，测试下能不能上传。

--- StandFord-Memorize2023/ContentView.swift
+++ StandFord-Memorize2023/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("好难啊呜呜呜呜")
         }
         .padding()
     }
