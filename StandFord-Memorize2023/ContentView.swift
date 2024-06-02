6e7cf47918bcf7549c34a0bb5c5914448fae7107
Wise Wong <imhwc@icloud.com>
2024-06-02 21:40:35 +0800

    1、卡片的数量会变化，所以无法使用常量数组的值，需要用变量 2、增加 了按钮支持卡片的增减，逻辑是通过卡片数值的改变而改变 3、并对卡片数值不超过 emoji 数值做了数据保护

--- StandFord-Memorize2023/ContentView.swift
+++ StandFord-Memorize2023/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👹","🤡","👻","👿",]
+    let emojis = ["👹","🤡","👻","👿","💀","☠️","👽","👾","🤖","🎃","👺","🤠"]
+    @State var cardsCount = 4
+    
     var body: some View {
-        HStack {
+        VStack{
+            HStack {
 
-            ForEach(emojis.indices,id: \.self) { index in
-               CardView(content: emojis[index])
-            }
+                ForEach(0..<cardsCount,id: \.self) { index in
+                CardView(content: emojis[index])
+                //ForEach的 range 使用 cardsCount 的初始值设定范围
+                //id:\.self,使用这个范围作为唯一标识符
+                //index 则为变量则为这个元素的索引
+                //in是语法的一部分，表示分隔前面的集合和后面的闭包参数，也指明将遍历集合每个参数所带来的元素值
+                }
            
+            }
+            .foregroundColor(.orange)
+        
+            HStack{
+                Button(action: {
+                    if cardsCount < emojis.count{
+                        cardsCount +=  1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.minus.fill")
+                    Text("添加卡片")
+                }).buttonStyle(.bordered)
+                Spacer()
+                Button(action: {
+                    if cardsCount > 1{
+                        cardsCount -=  1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.minus.fill")
+                    Text("删除卡片")
+                }).buttonStyle(.bordered)
+            }.font(.headline)
         }
-        .foregroundColor(.orange)
+       
+
+        
         .padding()
     }
 }
