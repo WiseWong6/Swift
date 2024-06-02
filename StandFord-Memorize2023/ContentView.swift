5fab9add2f8b9a5865a1b42b46909d01398df4a7
Wise Wong <imhwc@icloud.com>
2024-06-02 15:14:38 +0800

    Initial Commit

---
+++ StandFord-Memorize2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StandFord-Memorize2023
+//
+//  Created by Wise Wong on 2024/6/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StandFord-Memorize2023/StandFord_Memorize2023App.swift
@@ -0,0 +1,17 @@
+//
+//  StandFord_Memorize2023App.swift
+//  StandFord-Memorize2023
+//
+//  Created by Wise Wong on 2024/6/2.
+//
+
+import SwiftUI
+
+@main
+struct StandFord_Memorize2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
