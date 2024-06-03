01d3dfcb29b284f374b04f10fe562065fb710e89
Wise Wong <imhwc@icloud.com>
2024-06-03 11:35:41 +0800

    删除掉注释，将注释整理到笔记，提高代码可读性。

--- StandFord-Memorize2023/ContentView.swift
+++ StandFord-Memorize2023/ContentView.swift
@@ -23,18 +23,9 @@ struct ContentView: View {
     }
     var Cards:some View{
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120.0))], content: {
-            //LazyVGrid，是一种网格函数，columns 可以指定函数列，不能使用数字，要使用网格项数组
-            //GridItem 有一种自适应方法，但需要指定最小宽邸
-        
             ForEach(0..<cardsCount,id: \.self) { index in
             CardView(content: emojis[index])
-                .aspectRatio(2/3, contentMode: .fill)
-            //ForEach的 range 使用 cardsCount 的初始值设定范围
-            //id:\.self,使用这个范围作为唯一标识符
-            //index 则为变量则为这个元素的索引
-            //in是语法的一部分，表示分隔前面的集合和后面的闭包参数，也指明将遍历集合每个参数所带来的元素值
-            //aspectRatio是宽高比,含有参数 aspect ratio，2/3即宽度和高度的比例
-            //contentMode是内容模式，前侧指定了宽高比，.fill 则拉伸来完成这个宽高比,看最新语法只有 fill 和 fit，前者是填充，后者是缩放
+                .aspectRatio(4/3, contentMode: .fill)
             }
         }).foregroundColor(.orange)
     }
@@ -49,19 +40,15 @@ struct ContentView: View {
     }
     
     func ButtonSimplify(by offset:Int,symbol:String,txt:String)->some View{
-        //by 关键字通常用于指定一个修饰符（modifier）的参数，该修饰符会基于某个值的变化来应用某种效果或行为。
-        //offset单词指偏移量，并指定为整形，方法传递这个值给函数，然后根据这个值来创建按钮并运行逻辑。
         Button(action: {
             cardsCount +=  offset
-            //参考cardsCount +=  1，即 cardsCount=cardsCount+1，即现值+1，并重新赋值原值
-            //对应上文，则为 cardsCount+offset 的结果赋值原值
-            //假设cardsCount=4，传值 1，则为4+1=5，传值-1，则为 4-1=3
-            print("\(cardsCount)")
+            print("当前卡片数量是：\(cardsCount)个")
 
         }, label: {
             Image(systemName: symbol)
             Text(txt)
         }).buttonStyle(.bordered).disabled(cardsCount + offset > emojis.count || cardsCount + offset < 1)
+        
     }
     
     var CardAdder:some View{
